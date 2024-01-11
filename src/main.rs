ff3a262cc1db65dfa95ac75a421c140f05a605bf
Ukuma012 <Ukuma012@gmail.com>
2024-01-11 10:43:03 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,16 @@
-use rocket::form::validate;
-use rocket::serde::json::{json, Json, Value};
 use rocket::serde::Deserialize;
 use rocket::{get, launch, routes};
 use rocket_dyn_templates::{context, Template};
-
-#[derive(Deserialize)]
-pub struct NewUser {
-    user: NewUserData,
-}
-
-#[derive(Deserialize, Validate)]
-struct NewUserData {
-    #[validate(length(min = 1))]
-    username: Option<String>,
-    #[validate(email)]
-    email: Option<String>,
-    #[validate(length(min = 8))]
-    password: Option<String>,
-}
-
-#[post("/users", format = "json", data = "<new_user>")]
-pub async fn post_users(new_user: Json<NewUser>) -> Result<Value, Errors> {
-    let new_user = FieldValidator::validate(&new_user);
-    let username = extracter.extract("username", new_user.username);
-    let email = extracter.extract("email", new_user.email);
-    let password = extracter.extract("username", new_user.password);
-}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello World"
 }
 
-#[get("/id/<id>")]
-fn id(id: usize) -> Template {
-    Template::render(
-        "index",
-        context! {
-            id: id,
-        },
-    )
-}
-
 #[launch]
 fn rocket() -> _ {
     let rocket = rocket::build()
-        .mount("/", routes![index, id])
+        .mount("/", routes![index])
         .attach(Template::fairing());
 
     let figment = rocket.figment();
